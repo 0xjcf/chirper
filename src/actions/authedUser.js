c3cea4309d8cd36dc0a349314c9a2a62ac94a8be
JCFlores <jflores717@gmail.com>
2018-08-19 17:28:52 -0500

    fixed setAuthUser action to return id

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,8 @@
 export const SET_AUTHED_USER = "SET_AUTHED_USER";
 
-export function setAuthUser(authUser) {
+export function setAuthUser(id) {
   return {
     type: SET_AUTHED_USER,
-    authUser
+    id
   };
 }
