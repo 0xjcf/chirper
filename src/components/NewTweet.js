bf1c5533fb4bed2d054b0d0329629e5ac275d540
JCFlores <jflores717@gmail.com>
2018-08-20 20:51:06 -0500

    created action & reducer to add tweet

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { handleAddTweet } from "../actions/tweets";
 
 class NewTweet extends Component {
   state = { text: "" };
@@ -15,7 +16,9 @@ class NewTweet extends Component {
   handleSubmit = e => {
     e.preventDefault();
     const { text } = this.state;
-    console.log("New Tweet: ", text);
+    const { dispatch, id } = this.props;
+
+    dispatch(handleAddTweet(text, id));
 
     this.setState(() => ({
       text: ""
@@ -51,4 +54,4 @@ function mapStateToProps() {
   return {};
 }
 
-export default NewTweet;
+export default connect()(NewTweet);
