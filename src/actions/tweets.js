9530e7b236db4f0448a71d81f166ce6bf66e80f2
JCFlores <jflores717@gmail.com>
2018-08-27 15:30:27 -0500

    React router complete

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -48,10 +48,11 @@ export function handleToggleTweet(info) {
   return dispatch => {
     dispatch(toggleTweet(info));
 
-    return saveLikeToggle(info).catch(e => {
-      console.warn("Error in handleToggleTweet: ", e);
-      dispatch(toggleTweet(info)),
+    return saveLikeToggle(info)
+      .catch(e => {
+        console.warn("Error in handleToggleTweet: ", e)
+        dispatch(toggleTweet(info))
         alert("There was an error liking the tweet. Try again.");
-    });
+      });
   };
 }
